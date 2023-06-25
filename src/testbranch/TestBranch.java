02aa37c0a702132ceba4bb496604237128e3a86d
ENES-64 <enes.yurekli2002@gmail.com>
2023-06-25 19:06:56 -0400

    committing to a branch

---
+++ src/testbranch/TestBranch.java
@@ -0,0 +1,13 @@
+package testbranch;
+
+public class TestBranch {
+
+	public static void main(String[] args) {
+		
+		int x = 10, y = 5;
+		
+		System.out.println(x > y ? x : y + " is bigger.");
+		
+	}
+	
+}
